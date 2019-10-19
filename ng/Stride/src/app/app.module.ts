1339d2b84742862f58dba4e3463ce095a02142ee
Keyon-Jerome <keyonjerome@gmail.com>
2019-10-19 18:13:41 -0400

    Added components.

--- ng/Stride/src/app/app.module.ts
+++ ng/Stride/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ChooseABalloonComponent } from './choose-aballoon/choose-aballoon.component';
+import { BalloonComponent } from './balloon/balloon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChooseABalloonComponent,
+    BalloonComponent
   ],
   imports: [
     BrowserModule,
---
+++ ng/Stride/src/app/choose-aballoon/choose-aballoon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-aballoon',
+  templateUrl: './choose-aballoon.component.html',
+  styleUrls: ['./choose-aballoon.component.css']
+})
+export class ChooseABalloonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
